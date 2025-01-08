881c5d938d53324143c75cc05bca43d434990292
hola7671 <hola7671@gmail.com>
2025-01-09 03:09:05 +0800

    Envirnment setting finished.

---
+++ MyApplication/app/src/main/java/tw/edu/ncku/gs/r76121243/myapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package tw.edu.ncku.gs.r76121243.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
